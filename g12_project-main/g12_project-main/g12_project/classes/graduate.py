d5d82888cab31c9f8b6bdb88d991654f676f1829
mariaspteixeira08 <up202505257@g.uporto.pt>
2026-06-03 23:22:59 +0100

    Add files via upload

---
+++ g12_project-main/g12_project-main/g12_project/classes/graduate.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 17:09:19 2026
+
+@author: Tania Tavares
+"""
+from .gclass import Gclass
+
+
+class Graduate(Gclass):
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    att = ['_graduate_id', '_university_id', '_observations']
+    des = ['Graduate ID', 'University ID', 'Observations']
+
+    def __init__(self, graduate_id, university_id, observations):
+        super().__init__()
+        self._graduate_id = graduate_id
+        self._university_id = university_id
+        self._observations = observations
+
+        if self._graduate_id not in Graduate.obj:
+            Graduate.obj[self._graduate_id] = self
+            Graduate.lst.append(self._graduate_id)
+
+    @property
+    def graduate_id(self):
+        return self._graduate_id
+
+    @property
+    def university_id(self):
+        return self._university_id
+
+    @property
+    def observations(self):
+        return self._observations
+
+    def __str__(self):
+        return (f"Graduate(ID: {self._graduate_id}, "
+                f"Univ ID: {self._university_id}, "
+                f"Obs: {self._observations})")
