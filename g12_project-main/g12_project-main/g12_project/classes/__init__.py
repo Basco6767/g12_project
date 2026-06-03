73c6bd14a58bf70a41df09f15ded5935e6bd3826
mariaspteixeira08 <up202505257@g.uporto.pt>
2026-06-03 23:30:39 +0100

    Apagar pasta errada

--- g12_project-main/g12_project-main/g12_project/classes/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Pacote com as classes do modelo conceptual."""
-from .gclass import Gclass
-from .university import University
-from .graduate import Graduate
-from .association import Association
-from .membership import Membership
-
-__all__ = ["Gclass", "University", "Graduate", "Association", "Membership"]
